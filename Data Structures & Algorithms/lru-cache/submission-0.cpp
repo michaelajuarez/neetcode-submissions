c295b15d6eac144dfeca3442ef75bca71d190a16
Michael Juarez <michael@juarezfamily.com>
2026-09-06 21:13:28 -0700

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.cpp
@@ -0,0 +1,73 @@
+class Node {
+public:
+    int key;
+    int val;
+    Node* prev;
+    Node* next;
+
+    Node(int key, int val) {
+        this->key = key;
+        this->val = val;
+        this->prev = nullptr;
+        this->next = nullptr;
+    }
+};
+
+class LRUCache {
+private:
+    int capacity;
+    unordered_map<int, Node*> cache;
+    Node* left;
+    Node* right;
+
+    void remove(Node* node) {
+        Node* prev = node->prev;
+        Node* next = node->next;
+        prev->next = next;
+        next->prev = prev;
+    }
+
+    void insert(Node* node) {
+        Node* prev = right->prev;
+        prev->next = node;
+        node->prev = prev;
+        node->next = right;
+        right->prev = node;
+    }
+
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+        cache.clear();
+        left = new Node(0, 0);
+        right = new Node(0, 0);
+        left->next = right;
+        right->prev = left;
+    }
+    
+    int get(int key) {
+        if (cache.find(key) != cache.end()) {
+            Node* node = cache[key];
+            remove(node);
+            insert(node);
+            return node->val;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if (cache.find(key) != cache.end()) {
+            remove(cache[key]);
+        }
+        Node* newNode = new Node(key, value);
+        cache[key] = newNode;
+        insert(newNode);
+
+        if (cache.size() > capacity) {
+            Node* least = left->next;
+            remove(least);
+            cache.erase(least->key);
+            delete least;
+        }
+    }
+};
